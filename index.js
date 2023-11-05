4302fc9ce134a67437c869c0b295185355e01e20
Charlisteketee <charlisteketee@gmail.com>
2023-11-04 18:10:08 -0700

    create code adding sum of all numbers from N - N

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN === toN){
+    return fromN;
+  } else if (fromN === 0 && toN === 1) {
+    return 1;
+  } else if (fromN < toN) {
+    return fromN + sum(fromN + 1, toN);
+  }
+};
 
 module.exports = sum;
